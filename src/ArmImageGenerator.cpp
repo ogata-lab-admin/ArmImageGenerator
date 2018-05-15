549d8bc1b4602721687cee2cb0efeb996a877328
Kanamura24 <o.kanamura@gmail.com>
2018-05-15 17:56:34 +0900

    edit

--- src/ArmImageGenerator.cpp
+++ src/ArmImageGenerator.cpp
@@ -10,7 +10,9 @@
 #include "ArmImageGenerator.h"
 
 #include <iomanip>
+#include <fstream>
 #include <ctime>
+
 
 #define _USE_MATH_DEFINES
 #include <math.h>
@@ -146,204 +148,205 @@ RTC::ReturnCode_t ArmImageGenerator::onActivated(RTC::UniqueId ec_id)
 {
 	std::cout << "[ArmImageGenerator] Initializing Arm and Parameters" << std::endl;
 
-	//coil::TimeValue tv1(5.0);
-	//coil::sleep(tv1);
+	coil::TimeValue tv1(5.0);
+	coil::sleep(tv1);
 
 	std::cout << "[ArmImageGenerator] Waiting Arm Component is Activated....." << std::endl;
 	while (true) {
-		int ok_count = 0;
-		const RTC::PortProfile& pp = m_manipCommonPort.getPortProfile();
-		if (pp.connector_profiles.length() > 0) {
-			RTC::PortProfile_var pp0 = pp.connector_profiles[0].ports[0]->get_port_profile();
-			RTC::ComponentProfile_var cp0 = pp0->owner->get_component_profile();
-			if (std::string(cp0->type_name) == armimagegenerator_spec[1]) {
-				RTC::PortProfile_var pp1 = pp.connector_profiles[0].ports[1]->get_port_profile();
-				RTC::ComponentProfile_var cp1 = pp1->owner->get_component_profile();
-				RTC::ExecutionContext_var ec1 = pp1->owner->get_context(0);
-				if (ec1->get_component_state(pp1->owner) == RTC::LifeCycleState::ACTIVE_STATE) {
-					break;
-				}
-			}
-			else {
-				RTC::ExecutionContext_var ec0 = pp0->owner->get_context(0);
-				if (ec0->get_component_state(pp0->owner) == RTC::LifeCycleState::ACTIVE_STATE) {
-					break;
-				}
-			}
-		}
-	}
+      //int ok_count = 0;
+ 		const RTC::PortProfile& pp = m_manipCommonPort.getPortProfile();
+ 		if (pp.connector_profiles.length() > 0) {
+ 			RTC::PortProfile_var pp0 = pp.connector_profiles[0].ports[0]->get_port_profile();
+ 			RTC::ComponentProfile_var cp0 = pp0->owner->get_component_profile();
+ 			if (std::string(cp0->type_name) == armimagegenerator_spec[1]) {
+ 				RTC::PortProfile_var pp1 = pp.connector_profiles[0].ports[1]->get_port_profile();
+ 				RTC::ComponentProfile_var cp1 = pp1->owner->get_component_profile();
+ 				RTC::ExecutionContext_var ec1 = pp1->owner->get_context(0);
+ 				if (ec1->get_component_state(pp1->owner) == ACTIVE_STATE) {
+ 					break;
+ 				}
+ 			}
+ 			else {
+ 				RTC::ExecutionContext_var ec0 = pp0->owner->get_context(0);
+ 				if (ec0->get_component_state(pp0->owner) == ACTIVE_STATE) {
+ 					break;
+ 				}
+ 			}
+ 		}
+ 	}
 
 
-	m_manipCommon->servoON();
-	m_manipMiddle->setSpeedJoint(20);
+ 	m_manipCommon->servoON();
+ 	m_manipMiddle->setSpeedJoint(20);
 
-	m_jointPos->length(6);
-	m_jointPos[0] = 0;
-	m_jointPos[1] = 0;
-	m_jointPos[2] = M_PI/2;
-	m_jointPos[3] = 0;
-	m_jointPos[4] = M_PI/2;
-	m_jointPos[5] = 0;
+ 	m_jointPos->length(6);
+ 	m_jointPos[0] = 0;
+ 	m_jointPos[1] = 0;
+ 	m_jointPos[2] = M_PI/2;
+ 	m_jointPos[3] = 0;
+ 	m_jointPos[4] = M_PI/2;
+ 	m_jointPos[5] = 0;
 
-	//m_manipMiddle->movePTPJointAbs(m_jointPos);
-
-
-	coil::TimeValue tv(3.0);
-	coil::sleep(tv);
+ 	//m_manipMiddle->movePTPJointAbs(m_jointPos);
 
 
-	m_j0counter = m_j1counter = 0;
+ 	coil::TimeValue tv(3.0);
+ 	coil::sleep(tv);
 
-	m_sleepTime = coil::TimeValue(m_wait_interval);
-	std::cout << "[ArmImageGenerator] Wait " << m_sleepTime.sec() << "[sec], " << m_sleepTime.usec() << "[usec]" << std::endl;
+
+ 	m_j0counter = m_j1counter = 0;
+
+ 	m_sleepTime = coil::TimeValue(m_wait_interval);
+ 	std::cout << "[ArmImageGenerator] Wait " << m_sleepTime.sec() << "[sec], " << m_sleepTime.usec() << "[usec]" << std::endl;
 	
-	std::cout << "[ArmImageGenerator] Ready." << std::endl;
+ 	std::cout << "[ArmImageGenerator] Ready." << std::endl;
 
-	time_t now = std::time(nullptr);
-	struct tm* localNow = std::localtime(&now);
-	std::ostringstream ss;
-	ss << "log"
-		<< 1900 + localNow->tm_year
-		<< std::setw(2) << std::setfill('0') << localNow->tm_mon + 1
-		<< std::setw(2) << std::setfill('0') << localNow->tm_mday
-		<< std::setw(2) << std::setfill('0') << localNow->tm_hour
-		<< std::setw(2) << std::setfill('0') << localNow->tm_min
-		<< std::setw(2) << std::setfill('0') << localNow->tm_sec;
+ 	time_t now = std::time(NULL);
+ 	struct tm* localNow = std::localtime(&now);
+ 	std::ostringstream ss;
+ 	ss << "log"
+ 		<< 1900 + localNow->tm_year
+ 		<< std::setw(2) << std::setfill('0') << localNow->tm_mon + 1
+ 		<< std::setw(2) << std::setfill('0') << localNow->tm_mday
+ 		<< std::setw(2) << std::setfill('0') << localNow->tm_hour
+ 		<< std::setw(2) << std::setfill('0') << localNow->tm_min
+ 		<< std::setw(2) << std::setfill('0') << localNow->tm_sec;
 
-	m_logDir = ss.str();
-#ifdef WIN32
-	_mkdir(m_logDir.c_str());
-#else
-	mkdir(m_logDir.c_str(), 0777);
-#endif
+ 	m_logDir = ss.str();
+ #ifdef WIN32
+ 	_mkdir(m_logDir.c_str());
+ #else
+ 	mkdir(m_logDir.c_str(), 0777);
+ #endif
 
-	/*
-	std::ofstream configFile;
-	configFile.open(m_logDir + "/config.yaml", std::ofstream::out);
-	configFile << "j0min: " << m_j0min << std::endl;
-	configFile << "j1min: " << m_j1min << std::endl;
-	configFile << "j0max: " << m_j0max << std::endl;
-	configFile << "j1max: " << m_j1max << std::endl;
-	configFile << "j0step: " << m_j0step << std::endl;
-	configFile << "j1step: " << m_j1step << std::endl;
-	configFile << "wait_interval: " << m_wait_interval << std::endl;
-	configFile.close();
-	*/
-	m_JointLog.open(m_logDir + "/joints.csv", std::ofstream::out);
+ 	/*
+ 	std::ofstream configFile;
+ 	configFile.open(m_logDir + "/config.yaml", std::ofstream::out);
+ 	configFile << "j0min: " << m_j0min << std::endl;
+ 	configFile << "j1min: " << m_j1min << std::endl;
+ 	configFile << "j0max: " << m_j0max << std::endl;
+ 	configFile << "j1max: " << m_j1max << std::endl;
+ 	configFile << "j0step: " << m_j0step << std::endl;
+ 	configFile << "j1step: " << m_j1step << std::endl;
+ 	configFile << "wait_interval: " << m_wait_interval << std::endl;
+ 	configFile.close();
+ 	*/
+     std::string filename = m_logDir + "/joints.csv";
+ 	m_JointLog.open(filename.c_str(), std::ios::out);//std::ofstream::out);
 
-	m_JointLog << "x, y, theta, ImageFilename" << std::endl;
-	return RTC::RTC_OK;
-}
+ 	m_JointLog << "x, y, theta, ImageFilename" << std::endl;
+ 	return RTC::RTC_OK;
+ }
 
 
 
-RTC::ReturnCode_t ArmImageGenerator::onDeactivated(RTC::UniqueId ec_id)
-{
-	m_jointPos[0] = 0;
-	m_jointPos[1] = 0;
-	m_jointPos[2] = M_PI/2;
-	m_jointPos[4] = M_PI/2;
+ RTC::ReturnCode_t ArmImageGenerator::onDeactivated(RTC::UniqueId ec_id)
+ {
+ 	m_jointPos[0] = 0;
+ 	m_jointPos[1] = 0;
+ 	m_jointPos[2] = M_PI/2;
+ 	m_jointPos[4] = M_PI/2;
 	
-	m_manipMiddle->movePTPJointAbs(m_jointPos);
+ 	m_manipMiddle->movePTPJointAbs(m_jointPos);
   
-	m_JointLog.close();
+ 	m_JointLog.close();
 
-	coil::TimeValue tv(3.0);
-	coil::sleep(tv);
+ 	coil::TimeValue tv(3.0);
+ 	coil::sleep(tv);
 
-	m_manipCommon->servoOFF();
-	return RTC::RTC_OK;
-}
+ 	m_manipCommon->servoOFF();
+ 	return RTC::RTC_OK;
+ }
 
-double Uniform( void ){
-  return ((double)rand()+1.0)/((double)RAND_MAX+2.0);
-}
+ double Uniform( void ){
+   return ((double)rand()+1.0)/((double)RAND_MAX+2.0);
+ }
 
 
-RTC::ReturnCode_t ArmImageGenerator::onExecute(RTC::UniqueId ec_id)
-{
-  double xlimit[2] = {120, 240};
-  double ylimit[2] = {-120, 120};
-  double thlimit[2] = {-M_PI+1.0e-10, M_PI-1.0e-10};
+ RTC::ReturnCode_t ArmImageGenerator::onExecute(RTC::UniqueId ec_id)
+ {
+   double xlimit[2] = {120, 240};
+   double ylimit[2] = {-120, 120};
+   double thlimit[2] = {-M_PI+1.0e-10, M_PI-1.0e-10};
   
-  double x = Uniform() * (xlimit[1] - xlimit[0]) + xlimit[0];
-  double y = Uniform() * (ylimit[1] - ylimit[0]) + ylimit[0];
-  double th = Uniform() * (thlimit[1] - thlimit[0]) + thlimit[0];
+   double x = Uniform() * (xlimit[1] - xlimit[0]) + xlimit[0];
+   double y = Uniform() * (ylimit[1] - ylimit[0]) + ylimit[0];
+   double th = Uniform() * (thlimit[1] - thlimit[0]) + thlimit[0];
 
-  x /= 1000.0;
-  y /= 1000.0;
+   x /= 1000.0;
+   y /= 1000.0;
 
-  double z = 40 / 1000.0;
-  double z_min = 10 / 1000.0;
+   double z = 40 / 1000.0;
+   double z_min = 10 / 1000.0;
 
-  double s2 = sin(th);
-  double c2 = cos(th);
+   double s2 = sin(th);
+   double c2 = cos(th);
 
 
-  std::cout << "--------------------------------------------------" << std::endl;
+   std::cout << "--------------------------------------------------" << std::endl;
 
-  JARA_ARM::CarPosWithElbow carPos;
+   JARA_ARM::CarPosWithElbow carPos;
 
-  std::cout << "Reach (" << x << ", " << y << ", " << z << ")" << std::endl;
-  carPos.carPos[0][0] = -c2;  carPos.carPos[0][1] = s2; carPos.carPos[0][2] =  0.0; carPos.carPos[0][3] = x;
-  carPos.carPos[1][0] =  s2;  carPos.carPos[1][1] = c2; carPos.carPos[1][2] =  0.0; carPos.carPos[1][3] = y;
-  carPos.carPos[2][0] =  0.0; carPos.carPos[2][1] = 0; carPos.carPos[2][2] = -1.0; carPos.carPos[2][3] = z;
-  carPos.elbow = 1.0;
-  carPos.structFlag = 1;
-  JARA_ARM::RETURN_ID_var ret1 = m_manipMiddle->movePTPCartesianAbs(carPos);
-  coil::sleep(m_sleepTime);
+   std::cout << "Reach (" << x << ", " << y << ", " << z << ")" << std::endl;
+   carPos.carPos[0][0] = -c2;  carPos.carPos[0][1] = s2; carPos.carPos[0][2] =  0.0; carPos.carPos[0][3] = x;
+   carPos.carPos[1][0] =  s2;  carPos.carPos[1][1] = c2; carPos.carPos[1][2] =  0.0; carPos.carPos[1][3] = y;
+   carPos.carPos[2][0] =  0.0; carPos.carPos[2][1] = 0; carPos.carPos[2][2] = -1.0; carPos.carPos[2][3] = z;
+   carPos.elbow = 1.0;
+   carPos.structFlag = 1;
+   JARA_ARM::RETURN_ID_var ret1 = m_manipMiddle->movePTPCartesianAbs(carPos);
+   coil::sleep(m_sleepTime);
 
-  std::cout << "[ArmImageGenerator] Down" << std::endl;
-  carPos.carPos[2][3] = z_min;
-  ret1 = m_manipMiddle->movePTPCartesianAbs(carPos);
-  coil::sleep(m_sleepTime);
+   std::cout << "[ArmImageGenerator] Down" << std::endl;
+   carPos.carPos[2][3] = z_min;
+   ret1 = m_manipMiddle->movePTPCartesianAbs(carPos);
+   coil::sleep(m_sleepTime);
 
-  std::cout << "[ArmImageGenerator] Release" << std::endl;
-  m_manipMiddle->moveGripper(50);
-  coil::sleep(m_sleepTime);
+   std::cout << "[ArmImageGenerator] Release" << std::endl;
+   m_manipMiddle->moveGripper(50);
+   coil::sleep(m_sleepTime);
 
-  std::cout << "[ArmImageGenerator] Up" << std::endl;
-  carPos.carPos[2][3] = z;
-  ret1 = m_manipMiddle->movePTPCartesianAbs(carPos);
-  coil::sleep(m_sleepTime);
+   std::cout << "[ArmImageGenerator] Up" << std::endl;
+   carPos.carPos[2][3] = z;
+   ret1 = m_manipMiddle->movePTPCartesianAbs(carPos);
+   coil::sleep(m_sleepTime);
 
-  std::cout << "[ArmImageGenerator] Escape" << std::endl;
-  //  m_jointPos->length(6);
-  m_jointPos[0] = M_PI/2;
-  m_jointPos[1] = 0;
-  m_jointPos[2] = M_PI/2;
-  m_jointPos[3] = 0;
-  m_jointPos[4] = M_PI/2;
-  m_jointPos[5] = 0;
-  m_manipMiddle->movePTPJointAbs(m_jointPos);  
-  coil::sleep(m_sleepTime);
+   std::cout << "[ArmImageGenerator] Escape" << std::endl;
+   //  m_jointPos->length(6);
+   m_jointPos[0] = M_PI/2;
+   m_jointPos[1] = 0;
+   m_jointPos[2] = M_PI/2;
+   m_jointPos[3] = 0;
+   m_jointPos[4] = M_PI/2;
+   m_jointPos[5] = 0;
+   m_manipMiddle->movePTPJointAbs(m_jointPos);  
+   coil::sleep(m_sleepTime);
 
-  JARA_ARM::CarPosWithElbow_var actual(new JARA_ARM::CarPosWithElbow());
-  //JARA_ARM::RETURN_ID_var ret2 = m_manipCommon->getFeedbackPosJoint(actual);
-  JARA_ARM::RETURN_ID_var ret2 = m_manipMiddle->getFeedbackPosCartesian(actual);
+   JARA_ARM::CarPosWithElbow_var actual(new JARA_ARM::CarPosWithElbow());
+   //JARA_ARM::RETURN_ID_var ret2 = m_manipCommon->getFeedbackPosJoint(actual);
+   JARA_ARM::RETURN_ID_var ret2 = m_manipMiddle->getFeedbackPosCartesian(actual);
 
-  std::cout << "[ArmImageGenerator] Waiting for CameraImage...." << std::ends;
+   std::cout << "[ArmImageGenerator] Waiting for CameraImage...." << std::ends;
 
-  time_t now = std::time(nullptr);
-  struct tm* localNow = std::localtime(&now);
-  std::ostringstream ss;
-  ss 
-    << "image"
-    << 1900 + localNow->tm_year
-    << std::setw(2) << std::setfill('0') << localNow->tm_mon + 1
-    << std::setw(2) << std::setfill('0') << localNow->tm_mday
-    << std::setw(2) << std::setfill('0') << localNow->tm_hour
-    << std::setw(2) << std::setfill('0') << localNow->tm_min
-    << std::setw(2) << std::setfill('0') << localNow->tm_sec
-    << ".png";
+   time_t now = std::time(NULL);
+   struct tm* localNow = std::localtime(&now);
+   std::ostringstream ss;
+   ss 
+     << "image"
+     << 1900 + localNow->tm_year
+     << std::setw(2) << std::setfill('0') << localNow->tm_mon + 1
+     << std::setw(2) << std::setfill('0') << localNow->tm_mday
+     << std::setw(2) << std::setfill('0') << localNow->tm_hour
+     << std::setw(2) << std::setfill('0') << localNow->tm_min
+     << std::setw(2) << std::setfill('0') << localNow->tm_sec
+     << ".png";
   
-  std::string filename = ss.str();
+   std::string filename = ss.str();
 
 
-#if 1
-  /// Capture Image and Save
-  bool imageArrived = false;
-  long counter = 0;
+ #if 1
+   /// Capture Image and Save
+   bool imageArrived = false;
+      //long counter = 0;
   //Inport data check
   while (m_cameraIn.isNew() && (!imageArrived)) {
     m_cameraIn.read();
@@ -363,7 +366,7 @@ RTC::ReturnCode_t ArmImageGenerator::onExecute(RTC::UniqueId ec_id)
     m_buffer.create(height, width, CV_8UC1);
   
   long data_length = m_camera.data.image.raw_data.length();
-  long image_size = width * height * channels;
+  // long image_size = width * height * channels;
   
   if (m_camera.data.image.format == Img::CF_RGB) {
     for (int i = 0; i<height; ++i)
